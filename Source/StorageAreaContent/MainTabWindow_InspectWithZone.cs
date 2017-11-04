95d569fa6ace0784a4a7afbf1c3476deefa016f3
Varisha84 <Varisha84@users.noreply.github.com>
2017-11-04 12:49:39 +0100

    Upload v1.0

---
+++ Source/StorageAreaContent/MainTabWindow_InspectWithZone.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using Verse;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+namespace StorageAreaContent
+{
+    [StaticConstructorOnStartup]
+    public class MainTabWindow_InspectZone_Stockpile : MainTabWindow_Inspect
+    {
+        private ZoneStockpileInspectPaneFiller zoneInspectPanelFiller;
+
+        public MainTabWindow_InspectZone_Stockpile() : base()
+        {
+            Log.Message("New instance");
+            zoneInspectPanelFiller = new ZoneStockpileInspectPaneFiller();
+            
+        }
+
+        public override void DoWindowContents(Rect inRect)
+        {
+            base.DoWindowContents(inRect);
+
+            Zone_Stockpile selZone = ((ISelectable)Find.Selector.FirstSelectedObject) as Zone_Stockpile;
+            if (selZone != null)
+            {
+                Rect rect = inRect.AtZero();
+                rect.yMin += 26f;
+                DoZoneInspectContents(selZone, rect);
+            } else
+            {
+                zoneInspectPanelFiller.ResetData();
+            }
+        }
+
+        public void DoZoneInspectContents(Zone_Stockpile zone, Rect rect)
+        {
+            zoneInspectPanelFiller.DoPaneContentsFor(zone, rect);
+        }
+    }
+}
---
+++ Source/StorageAreaContent/ZoneStockpileInspectPaneFiller.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace StorageAreaContent
+
+{
+    public class ZoneStockpileInspectPaneFiller
+    {
+        private readonly HashSet<ThingDef> thingDefinitions;
+        private readonly Dictionary<ThingDef, int> summedUpThings;
+        private Zone_Stockpile lastZoneInspected;
+        private Vector2 scrollPosition;
+
+        public ZoneStockpileInspectPaneFiller()
+        {
+            lastZoneInspected = null;
+            scrollPosition = Vector2.zero;
+            summedUpThings = new Dictionary<ThingDef, int>();
+            IEnumerable<TreeNode_ThingCategory> categories = ThingCategoryNodeDatabase.AllThingCategoryNodes;
+            thingDefinitions = new HashSet<ThingDef>();
+
+            foreach (TreeNode_ThingCategory tc in categories)
+            {
+                foreach (ThingDef td in tc.catDef.DescendantThingDefs)
+                {
+                    thingDefinitions.Add(td);
+                }
+            }
+        }
+
+        public void DoPaneContentsFor(Zone_Stockpile zone, Rect rect)
+        {
+            if (lastZoneInspected != zone)
+            {
+                SumUpThings(zone);
+            }
+
+            try
+            {
+                GUI.BeginGroup(rect);
+                Text.Anchor = TextAnchor.MiddleLeft;
+                Text.WordWrap = false;
+                Text.Font = GameFont.Small;
+
+                Rect mainRect = new Rect(12f, 16f, rect.width - 24f, rect.height - 24f);
+                float height = 4f + (float)this.summedUpThings.Count * 28f;
+                Rect viewRect = new Rect(mainRect.x, mainRect.y, mainRect.width - 20f, height);
+                Widgets.BeginScrollView(mainRect, ref this.scrollPosition, viewRect, true);
+                float num = mainRect.y + 4f;
+                float num2 = this.scrollPosition.y - 28f;
+                float num3 = this.scrollPosition.y + mainRect.height;
+                bool success = false;
+
+                foreach (ThingDef tDef in summedUpThings.Keys)
+                {
+                    if (num > num2 && num < num3)
+                    {
+                        Rect rect2 = new Rect(mainRect.x + 6, num, viewRect.width, 28f);
+                        success = DrawDataRow(rect2, tDef);
+                    } else
+                    {
+                        num += 28f;
+                    }
+
+                    if (success)
+                    {
+                        num += 28f;
+                    }
+                }
+                Widgets.EndScrollView();
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorOnce(string.Concat(new object[]
+                {
+                    "Error in Mod StorageAreaContent: ZoneStockpileInspectPaneFiller#DoPaneContentsFor ",
+                    Find.Selector.FirstSelectedObject,
+                    ": ", ex.ToString()
+                }), this.GetHashCode());
+            }
+            finally
+            {
+                Text.WordWrap = true;
+                Text.Anchor = TextAnchor.UpperLeft;
+                GUI.EndGroup();
+            }
+        }
+
+        public void ResetData()
+        {
+            lastZoneInspected = null;
+            scrollPosition = Vector2.zero;
+        }
+
+        private bool DrawDataRow(Rect rect, ThingDef tDef)
+        {
+            bool result = true;
+            try
+            {
+                Rect rect1 = new Rect(rect.x, rect.y, 27f, 27f);
+                Widgets.ThingIcon(rect1, tDef);
+                Rect rect2 = new Rect(rect1.x + 35, rect.y, rect.width - rect1.width - 35, rect.height);
+                GUI.color = Color.white;
+                Widgets.Label(rect2, tDef.label + " x" + summedUpThings[tDef]);
+            } catch (Exception e)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        private void SumUpThings(Zone_Stockpile zone)
+        {
+            lastZoneInspected = zone;
+            summedUpThings.Clear();
+            IEnumerator<Thing> thingIt = zone.AllContainedThings.GetEnumerator();
+            int currentAmount;
+
+            foreach (Thing t in zone.AllContainedThings)
+            {
+                currentAmount = 0;
+
+                if ((t.def.uiIcon != BaseContent.BadTex) && (thingDefinitions.Contains(t.def)))
+                {
+                    if (summedUpThings.TryGetValue(t.def, out currentAmount))
+                    {
+                        summedUpThings[t.def] = currentAmount + t.stackCount;
+                    }
+                    else
+                    {
+                        summedUpThings.Add(t.def, t.stackCount);
+                    }
+                }
+            }
+        }
+    }
+}
