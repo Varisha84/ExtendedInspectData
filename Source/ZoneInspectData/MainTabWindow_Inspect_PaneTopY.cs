1e68eb84c4b8c24cf66941c6ceb697224e08a289
Varisha84 <marcuswuensche@gmail.com>
2018-12-28 23:30:08 +0100

    Integrated Harmony-Lib, Fixed RimHUD bug and fixed error with item list of storage items

---
+++ Source/ZoneInspectData/MainTabWindow_Inspect_PaneTopY.cs
@@ -0,0 +1,34 @@
+﻿using Harmony;
+using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace ExtendedInspectData
+{
+    [HarmonyPatch(typeof(MainTabWindow_Inspect), "PaneTopY", MethodType.Getter)]
+    internal class MainTabWindow_Inspect_PaneTopY
+    {
+       
+        static bool Prefix(MainTabWindow_Inspect __instance, ref float __result)
+        {
+            List<object> things = Find.Selector.SelectedObjects.FindAll(thing => (thing as Zone_Growing) != null);
+
+            if ((things.Count > 0) && (things.Count == Find.Selector.NumSelected))
+            {
+                __result = (float)UI.screenHeight - __instance.RequestedTabSize.y - 35f;
+                return false;
+            }
+            else
+            {
+                things = Find.Selector.SelectedObjects.FindAll(thing => (thing as Building_Storage) != null);
+                if ((things.Count > 0) && (things.Count == Find.Selector.NumSelected))
+                {
+                    __result = (float)UI.screenHeight - __instance.RequestedTabSize.y - 35f;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Source/ZoneInspectData/ZoneStockpileInspectPaneFiller.cs
+++ Source/ZoneInspectData/ZoneStockpileInspectPaneFiller.cs
@@ -4,7 +4,7 @@ using RimWorld;
 using UnityEngine;
 using Verse;
 
-namespace ZoneInspectData
+namespace ExtendedInspectData
 
 {
     public class ZoneStockpileInspectPaneFiller
